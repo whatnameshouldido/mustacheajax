5328dd073c3cb07c69b471c0afe34d35ee0557c2
whatnameshouldido <pobyjun@naver.com>
2024-11-07 18:39:14 +0900

    정상 실행이 안되서 계정 새로 만듦
    정상 실행이 안되서 계정 새로 만듦

--- mustachajax/src/main/java/com/studymavernspringboot/mustachajax/commons/dto/SearchAjaxDto.java
+++ mustachajax/src/main/java/com/studymavernspringboot/mustachajax/commons/dto/SearchAjaxDto.java
@@ -31,9 +31,7 @@ public class SearchAjaxDto {
     public void settingValues() {
         this.setOrderByWord( (this.getSortColumn() != null ? this.getSortColumn() : "id")
                 + " " + (this.getSortAscDsc() != null ? this.getSortAscDsc() : "DESC") );
-        // SQL select 문장의 ORDER BY 구문을 만들어 주는 역할
         if ( this.getRowsOnePage() == null ) {
-            // 한 페이지당 보여주는 행의 갯수
             this.setRowsOnePage(10);
         }
         if ( this.getPage() == null || this.getPage() <= 0 ) {
